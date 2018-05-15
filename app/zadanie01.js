681e53218004b4f7d7793185ebae135a6f74fbec
Maria Zawiązalec <maria.zawiazalec@sagiton.pl>
2018-05-15 16:56:20 +0200

    day 3, almost ready

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,16 @@
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
 
-//Twój kod
+const crypto = require('crypto');
+
+const algorithms = ['sha256', 'sha512', 'md5', 'rmd160'];
+
+const passwords = ['??TegoHasła', 'CodersLab', 'Node.js Szyfruje Pliki', 'Zaźółć Gęślą Jaźń', 'Moje Haslo 1@3!', '111#$((@)n','Dzisiaj Szyfruje 83'];
+
+for (let i = 0; i < algorithms.length; i++) {
+    for (let j = 0; j < passwords.length; j++) {
+        const hash = crypto.createHmac(algorithms[i], passwords[j])
+        .digest('hex');
+        const result = hash === MY_PWD_HASH ? `${algorithms[i]} + ${passwords[j]} = ${hash}` : 'keep trying';
+    console.log(result)
+    }
+}
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,16 @@
-//Twój kod
+const fs = require('fs');
+const crypto = require('crypto');
+
+const result = '4f7ae6569b55cb6275423ca1cdf31475e607da1d5204c110a58fb480c96e6eca';
+
+process.argv.forEach((val) => {
+    fs.readFile(val, 'utf8', (err, data) => {
+        if (err === null){
+            const hash = crypto.createHmac('sha256', data)
+                .digest('hex');
+            console.log(hash);
+        } else {
+            console.log('Błąd podczas odczytu pliku!', err);
+        }
+    });
+})
